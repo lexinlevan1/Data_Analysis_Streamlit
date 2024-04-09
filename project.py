87caf8c72564f701ce82ec67711f9095c88cc10b
lexinlevan1 <ldeang@umd.edu>
2024-04-09 10:11:36 -0400

    Created tab separation

--- project.py
+++ project.py
@@ -4,7 +4,7 @@ import streamlit as st
 import matplotlib as plt
 import seaborn as sns
 
-# ------ Configuring Page -------
+# ------ Configuring Main Page -------
 
 st.set_page_config(page_title= 'Average Time Spent By A User On Social Media',
                    page_icon= ':bar_chart:',
@@ -22,68 +22,75 @@ st.caption('Kaggle Dataset Link : https://www.kaggle.com/datasets/imyjoshua/aver
 # Replace 'Lifestlye' with Lifestyle due to misspelling
 df.replace({'Lifestlye':'Lifestyle'}, inplace = True)
 
-# ------ Sidebar Creation -------
 
-profession = st.sidebar.multiselect(
-    'Select the profession:',
-    options = df['profession'].unique(),
-    default = df['profession'].unique()
-)
+tab1, tab2 = st.tabs(['EDA', 'Visualization'])
+with tab1:
+    st.header('Exploratory Data Analysis')
+    
+        # ------ Sidebar Creation -------
 
-location = st.sidebar.multiselect(
-    'Select the location:',
-    options = df['location'].unique(),
-    default = df['location'].unique()
-)
+    profession = st.sidebar.multiselect(
+        'Select the profession:',
+        options = df['profession'].unique(),
+        default = df['profession'].unique()
+    )
 
-gender = st.sidebar.multiselect(
-    'Select the gender:',
-    options = df['gender'].unique(),
-    default = df['gender'].unique()
-)
+    location = st.sidebar.multiselect(
+        'Select the location:',
+        options = df['location'].unique(),
+        default = df['location'].unique()
+    )
 
-interests = st.sidebar.multiselect(
-    'Select the interests:',
-    options = df['interests'].unique(),
-    default = df['interests'].unique()
-)
+    gender = st.sidebar.multiselect(
+        'Select the gender:',
+        options = df['gender'].unique(),
+        default = df['gender'].unique()
+    )
 
-demographics = st.sidebar.multiselect(
-    'Select the demographics:',
-    options = df['demographics'].unique(),
-    default = df['demographics'].unique()
-)
-# *** Selection ***
-query_multiline = '''
-    profession == @profession &
-    location == @location &
-    gender == @gender &
-    interests == @interests &
-    demographics == @demographics
-'''
+    interests = st.sidebar.multiselect(
+        'Select the interests:',
+        options = df['interests'].unique(),
+        default = df['interests'].unique()
+    )
 
-query_multiline = query_multiline.replace('\n', '')
+    demographics = st.sidebar.multiselect(
+        'Select the demographics:',
+        options = df['demographics'].unique(),
+        default = df['demographics'].unique()
+    )
+    # *** Selection ***
+    query_multiline = '''
+        profession == @profession &
+        location == @location &
+        gender == @gender &
+        interests == @interests &
+        demographics == @demographics
+    '''
 
-# Customized Dataframe Based on selections
-df_selection = df.query(query_multiline)
+    query_multiline = query_multiline.replace('\n', '')
 
-st.dataframe(df_selection)
+    # Customized Dataframe Based on selections
+    df_selection = df.query(query_multiline)
 
-# ------ Main Page -------
-st.title(':bar_chart: Time Spent Correlations')
-st.markdown('###')
+    st.dataframe(df_selection)
 
-average_income = df_selection['income'].mean()
-average_time_spent = df_selection['time_spent'].mean()
-total_home_owners = len(df[df['isHomeOwner'] == False])
+with tab2: 
 
-left_col, middle_col, right_col = st.columns(3)
-with left_col: 
-    st.subheader(':dollar: Income Average:')
-    st.subheader(f'$ {average_income}')
-with middle_col: 
-    st.subheader(':clock2: Average Time')
-    st.subheader(f'{average_time_spent} hrs')
-with right_col: 
-    st.subheader(':house: Homeowners')
-    st.subheader(total_home_owners)
+    # ------ Main Page -------
+    st.title(':bar_chart: Time Spent Correlations')
+    st.markdown('###')
+
+    average_income = round(df_selection['income'].mean(),1)
+    average_time_spent = df_selection['time_spent'].mean()
+    total_home_owners = len(df[df['isHomeOwner'] == False])
+
+    left_col, middle_col, right_col = st.columns(3)
+    with left_col: 
+        st.subheader(':dollar: Income Average:')
+        st.subheader(f'$ {average_income}')
+    with middle_col: 
+        st.subheader(':clock2: Average Time')
+        st.subheader(f'{average_time_spent} hrs')
+    with right_col: 
+        st.subheader(':house: Homeowners')
+        st.subheader(total_home_owners)
